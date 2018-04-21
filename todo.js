63f30e435466892fe9f23d2c735a6714b192f663
nakano <goldraphaie@yahoo.co.jp>
2018-04-21 17:52:18 +0900

    Todoの削除の実装
    close #4

--- todo.js
+++ todo.js
@@ -14,14 +14,18 @@ function makeTodo(todoName) {
 	const checkbox_th = $('<th class="checkbox_th">').html(
 							$('<input>').attr('type', 'checkbox'))
 	const todoName_th = $('<th class="todoName_th">').html(todoName).dblclick((e)=>{onClickEdit($(e.target))})
-	const erase_th = $('<th class="erase_th">').html(
-						$('<input type="button">'))
+	const erase_th = $('<th class="erase_th">').html($('<input type="button">').click((e)=>{onErase($(e.target))}))
 	return $('<tr>').append(checkbox_th, todoName_th, erase_th)
 }
+///ï“èW
 function onClickEdit(target) {
 	target.html($('<input type="text" class="todo_edit">').val(target.text()).blur((e)=>{onBlurEdit($(e.target))}))
 	target.children().eq(0).focus();
 }
 function onBlurEdit(target) {
 	target.parent().html(target.val());
+}
+///çÌèú
+function onErase(target) {
+	target.parent().parent().remove();
 }
