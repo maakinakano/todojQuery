773bfe0053d3d2d4cb5fcdfccf9b74305999cfd0
nakano <goldraphaie@yahoo.co.jp>
2018-04-21 17:12:30 +0900

    Todoの編集の実装
    close #3

--- todo.js
+++ todo.js
@@ -13,8 +13,15 @@ function init() {
 function makeTodo(todoName) {
 	const checkbox_th = $('<th class="checkbox_th">').html(
 							$('<input>').attr('type', 'checkbox'))
-	const todoName_th = $('<th class="todoName_th">').html(todoName)
+	const todoName_th = $('<th class="todoName_th">').html(todoName).dblclick((e)=>{onClickEdit($(e.target))})
 	const erase_th = $('<th class="erase_th">').html(
 						$('<input type="button">'))
 	return $('<tr>').append(checkbox_th, todoName_th, erase_th)
+}
+function onClickEdit(target) {
+	target.html($('<input type="text" class="todo_edit">').val(target.text()).blur((e)=>{onBlurEdit($(e.target))}))
+	target.children().eq(0).focus();
+}
+function onBlurEdit(target) {
+	target.parent().html(target.val());
 }
